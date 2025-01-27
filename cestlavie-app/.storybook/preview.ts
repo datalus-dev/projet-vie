367fdf81566e17b2328e222e6d867065a9c9f803
Teon L Brooks <teon.brooks@gmail.com>
2025-01-27 16:08:51 -0500

    Upgrade storybook, Add daisyui
    https://storybook.js.org/docs/builders/vite
    - `npx storybook automigrate`
    
    also the addon was out of date so I needed to upgrade it
    - https://github.com/storybookjs/addon-svelte-csf/issues/233#issuecomment-2483157238

--- cestlavie-app/.storybook/preview.ts
+++ cestlavie-app/.storybook/preview.ts
@@ -1,14 +1,16 @@
 import type { Preview } from '@storybook/svelte';
 
 const preview: Preview = {
-	parameters: {
+    parameters: {
 		controls: {
 			matchers: {
 				color: /(background|color)$/i,
 				date: /Date$/i
 			}
 		}
-	}
+	},
+
+    tags: ['autodocs', 'autodocs', 'autodocs', 'autodocs']
 };
 
 export default preview;
