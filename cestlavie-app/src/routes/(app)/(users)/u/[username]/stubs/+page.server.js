a96e9c4f9fbe89c7f7e4c6440a057b0ca05f7a57
Teon L Brooks <teon.brooks@gmail.com>
2025-04-29 15:44:10 -0400

    Update +layout.svelte
    Update +page.server.js

--- cestlavie-app/src/routes/(app)/(users)/u/[username]/stubs/+page.server.js
+++ cestlavie-app/src/routes/(app)/(users)/u/[username]/stubs/+page.server.js
@@ -1,12 +1,6 @@
-// export const prerender = true
-
 export const load = async ({ url, fetch, locals: { supabase } }) => {
   const Res = await fetch(`${url.origin}/api/scrapbook/stub_text_extraction.toml`);
   const collection = await Res.json();
-
-  var pages = Object.keys(import.meta.glob('/static/images/thumbnails/stubs/*.jpg'));
-  // remove /static from the filepath
-  pages = pages.map((page) => page.slice(8));
 
   const { data: contents } = await supabase.from('content').select('content, contentType');
 
@@ -15,5 +9,5 @@ export const load = async ({ url, fetch, locals: { supabase } }) => {
     .getPublicUrl('images/thumbnails/stubs');
   const path = root.publicUrl;
 
-  return { collection, path, pages, contents };
+  return { collection, path, contents };
 };
--- cestlavie-app/src/routes/+layout.svelte
+++ cestlavie-app/src/routes/+layout.svelte
@@ -125,7 +125,7 @@
   </div>
   <div class="drawer-side">
     <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
-    <ul class="menu bg-base-200 text-base-content min-h-92/100 w-42 p-8">
+    <ul class="menu bg-base-200 text-base-content min-h-100/100 w-42 p-8">
       <!-- Sidebar content here -->
       {#each navItems as navItem}
         <!-- {#if user?.email || navItem.public} -->
@@ -134,8 +134,8 @@
       {/each}
     </ul>
     <!-- #TODO: Fix the footer position -->
-    <footer class="footer bg-base-200">
-      <p>a <a href="https://datalus.xyz"><em>datalus</em></a> endeavor.</p>
+    <footer class="footer bg-base-200 absolute bottom-5 flex justify-center">
+      <a href="https://datalus.xyz"><p class="text-center">a <em>datalus</em> endeavor.</p></a>
     </footer>
   </div>
 </div>
